9a9a4680c7747370cd5281021aae8324483bf27b
MarvelousAudio <52361841+MarvelousAudio@users.noreply.github.com>
2020-05-06 21:23:24 -0700

    push
    code works

---
+++ Chapter 22/Character-handling functions isdigit, isalpha, isalnum and isxdigit/Character-handling functions isdigit, isalpha, isalnum and isxdigit/main.cpp
@@ -0,0 +1,40 @@
+//
+//  main.cpp
+//  Character-handling functions isdigit, isalpha, isalnum and isxdigit
+//
+//  Created by ben haywood on 5/6/20.
+//  Copyright © 2020 ben haywood. All rights reserved.
+//
+
+#include <iostream>
+#include <cctype> // character-handeling function prototypes
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "According is isdigit:\n"
+    << (isdigit('8') ? "8 is a" : "8 is not a") << " digit\n"
+    << (isdigit('#') ? "# is a" : "# is not a") << " digit\n";
+        
+        
+    cout << "\nAccording to isalpha:\n"
+    << (isalpha('A') ? "A is a" : "A is not a") << " letter\n"
+    << (isalpha('b') ? "b is a" : "b is not a") << " letter\n"
+    << (isalpha('&') ? "& is a" : "& is not a") << " letter\n"
+    << (isalpha('4') ? "4 is a" : "4 is not a") << " letter\n";
+    
+    
+    cout << "\nAccording to isalnum:\n"
+    << (isalnum('A') ? "A is a" : "A is not a") << "digit or a letter\n"
+    << (isalnum('8') ? "8 is a" : "8 is not a") << "digit or a letter\n"
+    << (isalnum('#') ? "# is a" : "# is not a") << "digit or  a letter\n";
+    
+    
+    cout << "\nAccording to isxdigit:\n"
+    << (isxdigit('F') ? "F is a" : "F is not a") << "hexadecimal digit\n"
+    << (isxdigit('J') ? "J is a" : "J is not a") << "hexadecimal digit\n"
+    << (isxdigit('7') ? "7 is a" : "7 is not a") << "hexadecimal digit\n"
+    << (isxdigit('$') ? "$ is a" : "$ is not a") << "hexadecimal digit\n"
+    << (isxdigit('f') ? "f is a" : "$ is not a") << "hexadecimal digit" << endl;
+    return 0;
+}
