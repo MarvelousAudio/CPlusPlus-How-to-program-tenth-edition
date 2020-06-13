f3ffea9fce449f0779b6c8a645b852b25e598a04
MarvelousAudio <52361841+MarvelousAudio@users.noreply.github.com>
2020-06-13 01:44:34 -0700

    push
    code works

---
+++ Chapter 2/Printing multiple lines of text with a single statement/Printing multiple lines of text with a single statement/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Printing multiple lines of text with a single statement
+//
+//  Created by ben haywood on 6/13/20.
+//  Copyright © 2020 ben haywood. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Welcome\nto\n\nC++!\n";
+    
+    return 0;
+}
