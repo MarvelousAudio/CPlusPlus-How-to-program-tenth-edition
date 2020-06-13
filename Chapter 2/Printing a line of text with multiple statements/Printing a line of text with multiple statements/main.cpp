ee5d41757681b8e37bf4d4186ccb27788dfd4fb9
MarvelousAudio <52361841+MarvelousAudio@users.noreply.github.com>
2020-06-13 01:42:38 -0700

    push
    code works

---
+++ Chapter 2/Printing a line of text with multiple statements/Printing a line of text with multiple statements/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Printing a line of text with multiple statements
+//
+//  Created by ben haywood on 6/13/20.
+//  Copyright © 2020 ben haywood. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Welcome";
+    std::cout << "to C++!\n";
+    
+    return 0;
+}
---
+++ Chapter 2/Text-printing program/Text-printing program/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Text-printing program
+//
+//  Created by ben haywood on 6/13/20.
+//  Copyright © 2020 ben haywood. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Welcome to C++!\n"; // display message
+    
+    return 0;
+}
