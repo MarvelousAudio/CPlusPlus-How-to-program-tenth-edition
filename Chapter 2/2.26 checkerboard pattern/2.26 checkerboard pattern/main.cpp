b3d709a2eaf4a4de0e68644d5486539864f90e1b
MarvelousAudio <52361841+MarvelousAudio@users.noreply.github.com>
2020-06-20 15:11:58 -0700

    push
    code works

---
+++ Chapter 2/2.26 checkerboard pattern/2.26 checkerboard pattern/main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  2.26 checkerboard pattern
+//
+//  Created by ben haywood on 6/20/20.
+//  Copyright © 2020 ben haywood. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    
+    cout << "* * * * * * * * *" << endl;
+    cout << " * * * * * * * * *" << endl;
+    cout << "* * * * * * * * *" << endl;
+    cout << " * * * * * * * * *" << endl;
+    cout << "* * * * * * * * *" << endl;
+    cout << " * * * * * * * * *" << endl;
+    cout << "* * * * * * * * *" << endl;
+    cout << " * * * * * * * * *" << endl;
+    cout << endl;
+    return 0;
+}
