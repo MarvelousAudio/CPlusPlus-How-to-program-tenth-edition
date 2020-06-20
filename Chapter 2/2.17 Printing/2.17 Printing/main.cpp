996a2fcb3e3d8a0085d22f0f818e3385bc8c5382
MarvelousAudio <52361841+MarvelousAudio@users.noreply.github.com>
2020-06-19 22:06:01 -0700

    push
    code works

---
+++ Chapter 2/2.17 Printing/2.17 Printing/main.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  2.17 Printing
+//
+//  Created by ben haywood on 6/19/20.
+//  Copyright © 2020 ben haywood. All rights reserved.
+//
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    // a)
+    cout << "1 2 3 4" << endl;
+    
+    
+    // b)
+    cout << "1 " << "2 " << "3 " << "4" << endl;
+    
+    
+    // c)
+    cout << "1 ";
+    cout << "2 ";
+    cout << "3 ";
+    cout << "4" << endl;
+    
+    return 0;
+}
